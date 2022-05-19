0f8ccc07538cbd1a4e783527d00cbd18c1e8492f
glennleo <glennleo@Les-MacBook-Pro.local>
2022-05-19 15:54:40 +0700

    update github migration action workflow

--- contentful/migrations/2.js
+++ contentful/migrations/2.js
@@ -1,14 +1,16 @@
+const POST_TYPE = "post";
+const ENTRY_ID_FIELD = "entryId";
+
 // @ts-check
 /** @type {import('contentful-migration').MigrationFunction} */
 module.exports = async function (migration) {
-  const entryID = migration.editContentType("post").createField("entryID", {
+  const postType = migration.editContentType(POST_TYPE);
+  postType.createField(ENTRY_ID_FIELD, {
     name: "Entry ID",
     type: "Symbol",
     required: true,
   });
-  migration
-    .editContentType("post")
-    .changeFieldControl(entryID.id, "builtin", "slugEditor", {
-      trackingFieldId: "title",
-    });
+  postType.changeFieldControl(ENTRY_ID_FIELD, "builtin", "slugEditor", {
+    trackingFieldId: "title",
+  });
 };
