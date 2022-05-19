47ebc2e354a37cb2bcd2847f60f577ed85988f8c
glennleo <glennleo@Les-MacBook-Pro.local>
2022-05-19 11:48:10 +0700

    first commit

---
+++ components/post-header.js
@@ -0,0 +1,24 @@
+import Avatar from "../components/avatar";
+import DateComponent from "../components/date";
+import PostTitle from "../components/post-title";
+
+export default function PostHeader({ title, coverImage, date, author }) {
+  return (
+    <>
+      <div className="flex items-center">
+        <PostTitle>{title}</PostTitle>
+      </div>
+      <div className="hidden md:block md:mb-12">
+        {author && <Avatar name={author.name} picture={author.picture} />}
+      </div>
+      <div className="max-w-2xl mx-auto">
+        <div className="block md:hidden mb-6">
+          {author && <Avatar name={author.name} picture={author.picture} />}
+        </div>
+        <div className="mb-6 text-lg">
+          <DateComponent dateString={date} />
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ contentful/migrations/01_add_entry_field.js
@@ -0,0 +1,14 @@
+// @ts-check
+/** @type {import('contentful-migration').MigrationFunction} */
+module.exports = async function (migration) {
+  const entryID = migration.editContentType("Post").createField("entryID", {
+    name: "Entry ID",
+    type: "Symbol",
+    required: true,
+  });
+  migration
+    .editContentType("Post")
+    .changeFieldControl(entryID.id, "builtin", "slugEditor", {
+      trackingFieldId: "title",
+    });
+};
