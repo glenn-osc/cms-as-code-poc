1ac3074ae6b06223e5e153e4f0316d47d6a6210f
glennleo <glennleo@Les-MacBook-Pro.local>
2022-05-27 14:36:07 +0700

    migrate veresion 9

---
+++ migrations/9.js
@@ -0,0 +1,30 @@
+const ENTRY1_ID = "0eXAcCSeLEfvrSFqWBeUS";
+// @ts-check
+/** @type {import('contentful-migration').MigrationFunction} */
+module.exports = async function (migration, { makeRequest }) {
+  // Update entry title
+  await makeRequest({
+    method: "PATCH",
+    url: `/entries/${ENTRY1_ID}`,
+    headers: {
+      "Content-Type": "application/json-patch+json",
+      "X-Contentful-Version": 13,
+    },
+    data: [
+      {
+        op: "replace",
+        path: "/fields/title/en-US",
+        value: "Hello, Worlddddddddddddddd!",
+      },
+    ],
+  });
+  // Publish updated entry
+  await makeRequest({
+    method: "PUT",
+    url: `/entries/${ENTRY1_ID}/published`,
+    headers: {
+      "Content-Type": "application/json-patch+json",
+      "X-Contentful-Version": 13,
+    },
+  });
+};
